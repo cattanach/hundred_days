bbe68589c9c52412fe7281231b05ca72ec29625f
Sean Cattanach <sean.cattanach@live.com>
2022-04-08 01:10:45 +0100

    end of day 1

---
+++ 001/05_name_generator.py
@@ -0,0 +1,11 @@
+print("BNG v1.0")
+print("What's the name of the city you grew up in?")
+city = input()
+
+# OR
+# city = input("What's the name of the city you grew up in?\n")
+# \n creates prompt on next line
+
+print("What's your pet's name?")
+pet = input()
+print("Your band's name could be " + city + " " + pet)
