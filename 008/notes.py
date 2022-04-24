ba947f29d41348a72db0dafcc538237fe34cd0cf
Sean Cattanach <sean.cattanach@live.com>
2022-04-24 13:36:05 +0100

    middle of day 8

---
+++ 008/notes.py
@@ -0,0 +1,24 @@
+def function_1():
+    print("  This  ")
+    print(" is  a ")
+    print("function")
+
+function_1()
+
+def greet(name):
+    print(f"hey {name}")
+
+greet(input("what's your name\n"))
+print()
+
+# def my_function(something)          something = parameter | name of data being passed to function
+# my_function(123)                    123 = argument        | value of the data passed to function
+
+# functions with multiple inputs
+
+def greet_with_name_and_location(name, location):
+    print(f"hello {name}")
+    print(f"how's the weather in {location}?")
+
+greet_with_name_and_location("sean", "york")
+greet_with_name_and_location(location = "york", name = "sean") # keyword arguments
---
+++ 008/prime_checker.py
@@ -0,0 +1,31 @@
+# You need to write a function that checks whether if the number passed into it is a prime number or not.
+
+#Write your code below this line 👇
+
+import time
+
+def prime_checker(number):
+    
+    start = time.time() # time to execute function
+    
+    is_prime = True
+    for i in range(2,number):
+        if number % i == 0:
+            is_prime = False
+
+    if is_prime == True:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+    end = time.time()
+    total_time = int(end - start)
+
+    print(f"Execution time: {total_time} seconds")
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
